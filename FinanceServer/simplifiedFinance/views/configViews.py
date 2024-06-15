e6f520e8eaa44d51571dfbda3df1c9a353cd18ae
Pakos <pakostasip@gmail.com>
2024-06-15 13:42:32 +0200

    Securited form deleting config

--- FinanceServer/simplifiedFinance/views/configViews.py
+++ FinanceServer/simplifiedFinance/views/configViews.py
@@ -22,49 +22,54 @@ def isConfigured(reguest):
 @csrf_exempt
 def configure(request):
     try:
-        Transaction.objects.all().delete()
-        SubAccount.objects.all().delete()
-        Vendor.objects.all().delete()
-        Permissions.objects.all().delete()
-        Project.objects.all().delete()
-        Invoice.objects.all().delete()
-        User.objects.all().delete()
-
-        file = open(os.path.join(os.path.dirname(__file__), "..", "config.json"), "w")
-
-        organization = request.POST.get("organization")
-        address = request.POST.get("address")
-        NIP = request.POST.get("NIP")
-        accountNumber = request.POST.get("accountNumber")
-        accountBalance = request.POST.get("accountBalance")
-        treasurerName = request.POST.get("treasurerName")
-
-        treasurerLogin = request.POST.get("treasurerLogin")
-        treasurerPassword = request.POST.get("treasurerPassword")
-
-        config = {
-            "organization": organization,
-            "address": address,
-            "NIP": NIP,
-            "accountNumber": accountNumber,
-            "treasurerName": treasurerName,
-            "treasurerLogin": treasurerLogin
-        }
-
-        file.write(json.dumps(config))
+        file = open(os.path.join(os.path.dirname(__file__), "..", "config.json"), "r")
         file.close()
+        return JsonResponse({"error": "Configuration already exists"})
+    except FileNotFoundError:
+        try:
+            Transaction.objects.all().delete()
+            SubAccount.objects.all().delete()
+            Vendor.objects.all().delete()
+            Permissions.objects.all().delete()
+            Project.objects.all().delete()
+            Invoice.objects.all().delete()
+            User.objects.all().delete()
 
-        SubAccount.objects.create(
-            name="Główny rachunek",
-            accountNumber=accountNumber,
-            balance=accountBalance
-        )
+            file = open(os.path.join(os.path.dirname(__file__), "..", "config.json"), "w")
 
-        User.objects.create_superuser(username=treasurerLogin, password=treasurerPassword)
+            organization = request.POST.get("organization")
+            address = request.POST.get("address")
+            NIP = request.POST.get("NIP")
+            accountNumber = request.POST.get("accountNumber")
+            accountBalance = request.POST.get("accountBalance")
+            treasurerName = request.POST.get("treasurerName")
 
-        return JsonResponse({"success": True})
-    except Exception as e:
-        return JsonResponse({"success": False, "error": str(e)})
+            treasurerLogin = request.POST.get("treasurerLogin")
+            treasurerPassword = request.POST.get("treasurerPassword")
+
+            config = {
+                "organization": organization,
+                "address": address,
+                "NIP": NIP,
+                "accountNumber": accountNumber,
+                "treasurerName": treasurerName,
+                "treasurerLogin": treasurerLogin
+            }
+
+            file.write(json.dumps(config))
+            file.close()
+
+            SubAccount.objects.create(
+                name="Główny rachunek",
+                accountNumber=accountNumber,
+                balance=accountBalance
+            )
+
+            User.objects.create_superuser(username=treasurerLogin, password=treasurerPassword)
+
+            return JsonResponse({"success": True})
+        except Exception as e:
+            return JsonResponse({"success": False, "error": str(e)})
 
 
 @require_http_methods(["GET"])
