e0f26bcbb57b00ecb4b2ad50010c33dafb146586
Pakos <pakostasip@gmail.com>
2024-06-15 13:44:21 +0200

    Views for hondling users

--- FinanceServer/simplifiedFinance/urls.py
+++ FinanceServer/simplifiedFinance/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import configViews
+from .views import configViews, authViews
 
 
 urlpatterns = [
     path("isConfigured/", configViews.isConfigured),
     path("configure/", configViews.configure),
     path("getConfiguration/", configViews.getConfig),
+    path("login/", authViews.loginToApp),
+    path("logout/", authViews.logoutFromApp),
+    path("register/", authViews.registerUser),
+    path("session/", authViews.sessionInfo),
 ]
